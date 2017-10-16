3e19ce50db6eb3d4d1d553e2a89a9d81cd88ae41
LEVAI Daniel <leva@ecentrum.hu>
2017-10-16 21:06:06 +0200

    Use PATH_MAX instead of a local define
    Use PATH_MAX instead of a local define
    okk deraadt@ millert@ anton@
    Members:
    	c_ksh.c:1.50->1.51
    	path.c:1.18->1.19
    	sh.h:1.63->1.64
    
    Date: 2017/09/03 13:52:01
    Author: jca
    Patchset: 724

--- sh.h
+++ sh.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sh.h,v 1.63 2017/08/30 17:08:45 jca Exp $	*/
+/*	$OpenBSD: sh.h,v 1.64 2017/09/03 11:52:01 jca Exp $	*/
 
 /*
  * Public Domain Bourne/Korn shell
@@ -11,6 +11,7 @@
 /* Start of common headers */
 
 #include <fcntl.h>
+#include <limits.h>
 #include <setjmp.h>
 #define __USE_GNU /* needed for setresuid() and setresgid() */
 #include <stdarg.h>
@@ -35,7 +36,6 @@
 #define ISMAGIC(c)	((unsigned char)(c) == MAGIC)
 
 #define	LINE	4096		/* input line size */
-#define	PATH	1024		/* pathname size (todo: PATH_MAX/pathconf()) */
 
 extern	const char *kshname;	/* $0 */
 extern	pid_t	kshpid;		/* $$, shell pid */
